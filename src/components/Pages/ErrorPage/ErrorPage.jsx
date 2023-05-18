ca426735f65bbd9578bab22d7d281bdafc392a70
tamzid620 <tamzid620@gmail.com>
2023-05-18 20:26:40 +0600

    error page added

--- src/components/Pages/ErrorPage/ErrorPage.jsx
+++ src/components/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,15 @@
-
+import { Link } from "react-router-dom";
+import lottie from "../../../../public/errorpage.gif"
 
 const ErrorPage = () => {
     return (
         <div>
-            <h3>Error Page</h3>
+            <div className="flex justify-center items-center mb-10">
+                <img style={{ width: '500px' }} src={lottie} alt="" />
+            </div>
+            <div className="flex justify-center">
+                <Link className='px-8 py-3 font-semibold rounded bg-orange-400 text-black' to='/'>Back to homepage</Link>
+            </div>
         </div>
     );
 };
--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -2,38 +2,33 @@
 const Login = () => {
     return (
         <div>
-            <h1>Please Login Here</h1>
-            <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-purple-500 to-indigo-600">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+            <h1 className='flex justify-center mb-20 mt-10 font-bold text-4xl text-orange-400'>Please Login Here</h1>
+                <div className="flex justify-center ">
+      <div className="w-1/2 bg-gradient-to-br from-orange-200 to-orange-400 p-8 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold mb-6">Login</h2>
         <form>
           <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+            <label htmlFor="email" className="block mb-2 font-medium">
               Email
             </label>
             <input
               type="email"
               id="email"
-              className="w-full px-3 py-2 rounded-md border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
-              placeholder="Enter your email"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
             />
           </div>
-          <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+          <div className="mb-4">
+            <label htmlFor="password" className="block mb-2 font-medium">
               Password
             </label>
             <input
               type="password"
               id="password"
-              className="w-full px-3 py-2 rounded-md border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
-              placeholder="Enter your password"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-400"
             />
           </div>
-          <button
-            type="submit"
-            className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-          >
-            Sign In
+          <button type="submit" className="w-full font-bold bg-orange-500 text-white rounded-lg py-2 px-4 hover:bg-orange-600 transition duration-300">
+            Log in
           </button>
         </form>
       </div>
